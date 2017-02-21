cf1fb71d622ec44a91be7bc55e1d263628b91ef1
jonahwu <jonahjun.wu@gmail.com>
2017-02-21 10:55:15 +0800

    adding gps client with get

--- client/gpsrun.sh
+++ client/gpsrun.sh
@@ -1,4 +1,5 @@
-#go run client-arg.go gps.go -runcommand gpsloc
+#go run client-arg.go gps.go -runcommand gpsloc 3cd59439-98cf-4548-ba54-cb263f965dc7
 #go run client-arg.go gps.go -runcommand=runlogin kala kala
 # now the expred time has been changed to infinity
-curl -X POST -H "Auth-Token:cbcc7327-5222-4d89-a9f8-47ea40d357b3" -H "lati:23.333" -H "long:123.3335" http://localhost:8080/setgpsloc
+#curl -v -X GET -H "clientuserid:kala" -H "Auth-Token:3cd59439-98cf-4548-ba54-cb263f965dc7" http://localhost:8080/getgpsloc
+curl -X POST -H "Auth-Token:3cd59439-98cf-4548-ba54-cb263f965dc7" -H "lati:23.333" -H "long:123.3335" http://localhost:8080/setgpsloc
